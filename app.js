875364a9b35493d5d96eb919a9732d7086ed52b6
a-nshul <upadhyay041298@gmail.com>
2024-09-28 17:54:29 +0530

    deployement

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const path = require("path");
-const swaggerSetup = require('./swagger'); // Import Swagger setup
+const swaggerSetup = require('./swagger'); // Swagger setup
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const leaveRoutes = require("./routes/leaveRoutes");
-const listHolidayRoutes = require('./routes/listHolidayRoutes');
-const salaryRoutes = require('./routes/salaryRoutes');
+const listHolidayRoutes = require("./routes/listHolidayRoutes");
+const salaryRoutes = require("./routes/salaryRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
 
+// Initialize Express app
 const app = express();
 
 // Middleware
@@ -21,8 +22,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Swagger setup
-swaggerSetup(app); // Make sure this is called before your routes
+console.log("Setting up Swagger at /api-docs");
+
+swaggerSetup(app);
+
+console.log("Swagger setup complete");
 
 // API routes
 app.use("/api/user", userRoutes);
