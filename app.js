5269777e6e76059c22a25a41df3a91dc0c18d042
a-nshul <upadhyay041298@gmail.com>
2024-09-29 15:56:30 +0530

    deployement

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const path = require("path");
-const swaggerSetup = require('./swagger'); // Import Swagger setup
+// const swaggerSetup = require('./swagger'); // Import Swagger setup
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const profileRoutes = require("./routes/profileRoutes");
@@ -17,17 +17,28 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors(
+  {
+    origin: ["http://localhost:3000", "https://etimeflow-api-uinf.vercel.app"], // Add your frontend URL here
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["X-Auth-Token"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }
+))
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Swagger setup
-// swaggerSetup(app); // Make sure this is called before your routes
-console.log("Setting up Swagger at /api-docs");
+// // swaggerSetup(app); // Make sure this is called before your routes
+// console.log("Setting up Swagger at /api-docs");
 
-swaggerSetup(app);
+// swaggerSetup(app);
 
-console.log("Swagger setup complete");
+// console.log("Swagger setup complete");
 
 
 // API routes
